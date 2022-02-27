fd102018ad0bff864e53a4eac68b9459cafd9b95
christer_vesterlund <chrved@gmail.com>
2022-02-27 10:49:47 +0100

    Start

---
+++ src/main/java/chr/ved/tarantula/TarantulaApplication.java
@@ -0,0 +1,13 @@
+package chr.ved.tarantula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarantulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarantulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chr/ved/tarantula/TarantulaApplicationTests.java
@@ -0,0 +1,13 @@
+package chr.ved.tarantula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarantulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
